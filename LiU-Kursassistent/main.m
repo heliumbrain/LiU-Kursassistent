f8fbadfe264228d61e6258eede1bbc579130aa57
Nils Kanevad <nils@kanevad.se>
2012-11-12 13:27:44 +0100

    Projektet skapat!
    NU KÖR VI

---
+++ LiU-Kursassistent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LiU-Kursassistent
+//
+//  Created by Nils Kanevad on 11/12/12.
+//  Copyright (c) 2012 Nils Kanevad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
