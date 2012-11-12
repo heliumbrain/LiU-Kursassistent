bfd1d0c38a1f2acaf007103631832267fc97143e
Viktor Leviné <viktorlevine@live.se>
2012-11-12 14:00:20 +0100

    Klass för deadline tillagd

---
+++ LiU-Kursassistent/Deadline.h
@@ -0,0 +1,13 @@
+//
+//  Deadline.h
+//  LiU-Kursassistent
+//
+//  Created by Viktor Leviné on 2012-11-12.
+//  Copyright (c) 2012 Nils Kanevad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deadline : NSObject
+
+@end
---
+++ LiU-Kursassistent/Deadline.m
@@ -0,0 +1,13 @@
+//
+//  Deadline.m
+//  LiU-Kursassistent
+//
+//  Created by Viktor Leviné on 2012-11-12.
+//  Copyright (c) 2012 Nils Kanevad. All rights reserved.
+//
+
+#import "Deadline.h"
+
+@implementation Deadline
+
+@end
