5376d602a6198602d669913bcea766aa16a65e81
Nils Kanevad <nils@kanevad.se>
2012-12-03 15:58:38 +0100

    ViewControllers tillagda

---
+++ LiU-Kursassistent/ModalViewControllerAddCourse.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewControllerAddCourse.h
+//  LiU-Kursassistent
+//
+//  Created by Nils Kanevad on 12/3/12.
+//  Copyright (c) 2012 Nils Kanevad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewControllerAddCourse : UIViewController
+
+@end
---
+++ LiU-Kursassistent/ModalViewControllerAddCourse.m
@@ -0,0 +1,38 @@
+//
+//  ModalViewControllerAddCourse.m
+//  LiU-Kursassistent
+//
+//  Created by Nils Kanevad on 12/3/12.
+//  Copyright (c) 2012 Nils Kanevad. All rights reserved.
+//
+
+#import "ModalViewControllerAddCourse.h"
+
+@interface ModalViewControllerAddCourse ()
+
+@end
+
+@implementation ModalViewControllerAddCourse
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
